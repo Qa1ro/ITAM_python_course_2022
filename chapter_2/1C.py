aa93d6b91d477a226b09d4279d49e0fbc4ccc23a
Alex <114597291+Qa1ro@users.noreply.github.com>
2022-10-18 21:48:45 +0300

    1C.py

--- chapter_2/1C.py
+++ chapter_2/1C.py
@@ -1,6 +1,6 @@
-a=lambda x: -x*2 if x<0 else x
 def summation(*args):
-    x=list(map(a,list(args)))
-    mass=[i/max(x) for i in x]
+    a = lambda x: -x * 2 if x < 0 else x
+    b=list(map(a,list(args)))
+    mass=[i/max(b) for i in b]
     return mass
 print(summation(-1,3,4,-25,5))
